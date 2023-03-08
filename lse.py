13cecd2c8e9241242ddbccd44cd4941fec001df1
RenuReddyK <68454938+RenuReddyK@users.noreply.github.com>
2023-03-08 13:16:36 -0500

    Update lse.py

--- lse.py
+++ lse.py
@@ -1,8 +1,7 @@
 import numpy as np
 
 def least_squares_estimation(X1, X2):
-  """ YOUR CODE HERE
-  """
+
   N = X1.shape[0]
   A = np.zeros((N,9))
   a =[]
@@ -14,38 +13,16 @@ def least_squares_estimation(X1, X2):
       ay = py * X2[i,:]
       az = pz * X2[i,:]
       a = np.hstack((ax,ay,az))
-      # print(a)
       A[i,:9] = a
-
-  #print("A",A)
-  #print(A.shape)
-
-  # A=a
-  # print(A)
-  # a = np.matmul(A.T,A)
-  # print("a",a)
+  
   u, d, vt = np.linalg.svd(A)
-  # print(vt)
   E_dash = vt[8,:]
-  #print("E_dash",E_dash)
-  #print("vt",vt)
   E_dash = E_dash.reshape((3,3))
-  # print("E_dash",E_dash)
-  # print(E_dash.shape)
-  U, D, Vt = np.linalg.svd(E_dash.T)
-  # print(U.shape)
-  # print(Vt.shape)
-  E = np.matmul(np.matmul(U,np.diag([1,1,0])),Vt)
-  # print("E",E)
-  # print(E.shape)
-  # v=np.arange(9)
-  # print(v)
-  # A=v.reshape(3,3)
-  # print(A)
-  # print(A)
 
-  """ END YOUR CODE
-  """
+  U, D, Vt = np.linalg.svd(E_dash.T)
+
+  E = np.matmul(np.matmul(U,np.diag([1,1,0])),Vt)
+
   return E
 
 if __name__ == '__main__':
